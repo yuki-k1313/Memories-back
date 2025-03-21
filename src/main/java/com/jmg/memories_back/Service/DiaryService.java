f019827f9a726d8afda659ee5f62cc78141629e6
yuki-k1313 <yuki1313@naver.com>
2025-03-21 09:28:11 +0900

    2025-03-18

--- src/main/java/com/jmg/memories_back/Service/DiaryService.java
+++ src/main/java/com/jmg/memories_back/Service/DiaryService.java
@@ -2,11 +2,11 @@ package com.jmg.memories_back.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.jmg.memories_back.common.dto.response.diary.GetMyDiaryResponseDto;
+import com.jmg.memories_back.common.dto.response.diary.GetDiaryResponseDto;
+import com.jmg.memories_back.common.dto.response.ResponseDto;
 import com.jmg.memories_back.common.dto.request.diary.PatchDiaryRequestDto;
 import com.jmg.memories_back.common.dto.request.diary.PostDiaryRequestDto;
-import com.jmg.memories_back.common.dto.response.ResponseDto;
-import com.jmg.memories_back.common.dto.response.diary.GetDiaryResponseDto;
-import com.jmg.memories_back.common.dto.response.diary.GetMyDiaryResponseDto;
 
 public interface DiaryService {
     ResponseEntity<ResponseDto> postDiary(PostDiaryRequestDto dto, String userId);
