bc78d6abe482bb33c4bc5bd7cfeb6d6391f37949
yuki-k1313 <yuki1313@naver.com>
2025-03-26 15:55:27 +0900

    2025-03-18

--- src/main/java/com/jmg/memories_back/common/dto/request/auth/SignUpRequestDto.java
+++ src/main/java/com/jmg/memories_back/common/dto/request/auth/SignUpRequestDto.java
@@ -13,27 +13,27 @@ import lombok.Setter;
 @AllArgsConstructor
 public class SignUpRequestDto {
 
-    @NotBlank
-    @Pattern(regexp="^[a-zA-Z0-9]{6,20}$")
-    private String userId;
-    
-    @NotBlank
-    @Pattern(regexp="^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z-0-9]{8,13}$")
-    private String userPassword;
+  @NotBlank
+  @Pattern(regexp="^[a-zA-Z0-9]{6,20}$")
+  private String userId;
 
-    @NotBlank
-    @Pattern(regexp="^[가-힣]{2,5}$")
-    private String name;
-    
-    @NotBlank
-    private String address;
-    
-    private String detailAddress;
-    
-    @NotBlank
-    @Pattern(regexp="^(NORMAL|KAKAO|NAVER)$")
-    private String joinType;
+  @NotBlank
+  @Pattern(regexp="^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,13}$")
+  private String userPassword;
 
-    private String snsId;
+  @NotBlank
+  @Pattern(regexp="^[가-힣]{2,5}$")
+  private String name;
+
+  @NotBlank
+  private String address;
+
+  private String detailAddress;
+  
+  @NotBlank
+  @Pattern(regexp="^(NORMAL|KAKAO|NAVER)$")
+  private String joinType;
+
+  private String snsId;
 
 }
