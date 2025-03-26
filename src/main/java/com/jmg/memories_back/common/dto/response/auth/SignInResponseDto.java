3ad3dc4b221278b9d17557ef0b2e303ffcb8b9e7
yuki-k1313 <yuki1313@naver.com>
2025-03-26 15:59:05 +0900

    2025-03-14

--- src/main/java/com/jmg/memories_back/common/dto/response/auth/SignInResponseDto.java
+++ src/main/java/com/jmg/memories_back/common/dto/response/auth/SignInResponseDto.java
@@ -10,17 +10,17 @@ import lombok.Getter;
 @Getter
 public class SignInResponseDto extends ResponseDto {
 
-    private String accessToken;
-    private Integer expiration;
+  private String accessToken;
+  private Integer expiration;
 
-    private SignInResponseDto(String accessToken) {
-        this.accessToken = accessToken;
-        this.expiration = 60 * 60 * 9;
-    }
+  private SignInResponseDto(String accessToken) {
+    this.accessToken = accessToken;
+    this.expiration = 60 * 60 * 9;
+  }
 
-    public static ResponseEntity<SignInResponseDto> success(String accessToken) {
-        SignInResponseDto body = new SignInResponseDto(accessToken);
-        return ResponseEntity.status(HttpStatus.OK).body(body);
-    }
-
+  public static ResponseEntity<SignInResponseDto> success(String accessToken) {
+    SignInResponseDto body = new SignInResponseDto(accessToken);
+    return ResponseEntity.status(HttpStatus.OK).body(body);
+  }
+  
 }
