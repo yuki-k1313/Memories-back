fc511a19cb260ce0061fb59f9d2645ae0bbc39ed
yuki-k1313 <yuki1313@naver.com>
2025-03-26 16:00:34 +0900

    2025-03-20

--- src/main/java/com/jmg/memories_back/common/dto/response/diary/GetDiaryResponseDto.java
+++ src/main/java/com/jmg/memories_back/common/dto/response/diary/GetDiaryResponseDto.java
@@ -10,25 +10,24 @@ import lombok.Getter;
 
 @Getter
 public class GetDiaryResponseDto extends ResponseDto {
-    private String writeDate;
-    private String weather;
-    private String feeling;
-    private String title;
-    private String content;
-    private String writerId;
+  private String writeDate;
+  private String weather;
+  private String feeling;
+  private String title;
+  private String content;
+  private String writerId;
 
-    private GetDiaryResponseDto(DiaryEntity diaryEntity) {
-        this.writeDate = diaryEntity.getWriteDate();
-        this.weather = diaryEntity.getWeather();
-        this.feeling = diaryEntity.getFeeling();
-        this.title = diaryEntity.getTitle();
-        this.content = diaryEntity.getContent();
-        this.writerId = diaryEntity.getUserId();
-    }
+  private GetDiaryResponseDto(DiaryEntity diaryEntity) {
+    this.writeDate = diaryEntity.getWriteDate();
+    this.weather = diaryEntity.getWeather();
+    this.feeling = diaryEntity.getFeeling();
+    this.title = diaryEntity.getTitle();
+    this.content = diaryEntity.getContent();
+    this.writerId = diaryEntity.getUserId();
+  }
 
-    public static ResponseEntity<GetDiaryResponseDto> success(DiaryEntity diaryEntity) {
-        GetDiaryResponseDto body = new GetDiaryResponseDto(diaryEntity);
-        return ResponseEntity.status(HttpStatus.OK).body(body);
-    }
-
+  public static ResponseEntity<GetDiaryResponseDto> success(DiaryEntity diaryEntity) {
+    GetDiaryResponseDto body = new GetDiaryResponseDto(diaryEntity);
+    return ResponseEntity.status(HttpStatus.OK).body(body);
+  }
 }
--- src/main/java/com/jmg/memories_back/common/dto/response/user/GetSignInUserResponseDto.java
+++ src/main/java/com/jmg/memories_back/common/dto/response/user/GetSignInUserResponseDto.java
@@ -9,27 +9,27 @@ import com.jmg.memories_back.common.entity.UserEntity;
 import lombok.Getter;
 
 @Getter
-public class GetSignInUserResponseDto extends ResponseDto{
-    private String userId;
-    private String name;
-    private String profileImage;
-    private String address;
-    private String detailAddress;
-    private String gender;
-    private Integer age;
+public class GetSignInUserResponseDto extends ResponseDto {
+  private String userId;
+  private String name;
+  private String profileImage;
+  private String address;
+  private String detailAddress;
+  private String gender;
+  private Integer age;
 
-    private GetSignInUserResponseDto(UserEntity userEntity) {
-        this.userId = userEntity.getUserId();
-        this.name = userEntity.getName();
-        this.profileImage = userEntity.getProfileImage();
-        this.address = userEntity.getAddress();
-        this.detailAddress = userEntity.getDetailAddress();
-        this.gender = userEntity.getGender();
-        this.age = userEntity.getAge();
-    }
+  private GetSignInUserResponseDto(UserEntity userEntity) {
+    this.userId = userEntity.getUserId();
+    this.name = userEntity.getName();
+    this.profileImage = userEntity.getProfileImage();
+    this.address = userEntity.getAddress();
+    this.detailAddress = userEntity.getDetailAddress();
+    this.gender = userEntity.getGender();
+    this.age = userEntity.getAge();
+  }
 
-    public static ResponseEntity<GetSignInUserResponseDto> success(UserEntity userEntity) {
-        GetSignInUserResponseDto body = new GetSignInUserResponseDto(userEntity);
-        return ResponseEntity.status(HttpStatus.OK).body(body);
-    }
+  public static ResponseEntity<GetSignInUserResponseDto> success(UserEntity userEntity) {
+    GetSignInUserResponseDto body = new GetSignInUserResponseDto(userEntity);
+    return ResponseEntity.status(HttpStatus.OK).body(body);
+  }
 }
