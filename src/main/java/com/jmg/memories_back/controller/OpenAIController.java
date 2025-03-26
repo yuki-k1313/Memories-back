eaaf3ad0343e68aa829d29d7006ecba87a1b6a23
yuki-k1313 <yuki1313@naver.com>
2025-03-26 15:03:59 +0900

    2025-03-26

--- src/main/java/com/jmg/memories_back/controller/OpenAIController.java
+++ src/main/java/com/jmg/memories_back/controller/OpenAIController.java
@@ -2,7 +2,6 @@ package com.jmg.memories_back.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
