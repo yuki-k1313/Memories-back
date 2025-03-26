67e221cb5f2656c24620d67be457b1de226e6a08
yuki-k1313 <yuki1313@naver.com>
2025-03-26 15:45:19 +0900

    2025-03-18

--- src/main/java/com/jmg/memories_back/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/jmg/memories_back/handler/OAuth2SuccessHandler.java
@@ -19,50 +19,48 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    @Value("${oauth.client-main}")
-    private String oAuthClientMain;
-    @Value("${oauth.client-auth}")
-    private String oAuthClientAuth;
+  @Value("${oauth.client-main}")
+  private String oAuthClientMain;
+  @Value("${oauth.client-auth}")
+  private String oAuthClientAuth;
+  
+  @Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+    Map<String, Object> attributes = oAuth2User.getAttributes();
+    boolean existed = oAuth2User.isExisted();
 
-    @Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) 
-    throws IOException, ServletException {
-        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        boolean existed = oAuth2User.isExisted();
+    // description: 회원가입 O //
+    if (existed) {
+      String accessToken = (String) attributes.get("accessToken");
+      Cookie cookie = new Cookie("accessToken", accessToken);
+      cookie.setPath("/");
+      cookie.setHttpOnly(false);
+      cookie.setMaxAge(60 * 60 * 9);
 
-        // description: 회원가입 O //
-        if(existed) {
-            String accessToken = (String) attributes.get("accessToken");
-            Cookie cookie = new Cookie("accessToken", accessToken);
-            cookie.setPath("/");
-            cookie.setHttpOnly(false);
-            cookie.setMaxAge(60 * 60 * 9);
-
-            response.addCookie(cookie);
-            response.sendRedirect(oAuthClientMain);
-        }
-        // description: 회원가입 X // 
-        else {
-            String snsId = (String) attributes.get("snsId");
-            Cookie snsIdCookie = new Cookie("snsId", snsId);
-            snsIdCookie.setPath("/");
-            snsIdCookie.setHttpOnly(false);
-            snsIdCookie.setMaxAge(60 * 60);
-
-            response.addCookie(snsIdCookie);
-
-            String joinType = (String) attributes.get("joinType");
-            Cookie joinTypeCookie = new Cookie("joinType", joinType);
-            joinTypeCookie.setPath("/");
-            joinTypeCookie.setHttpOnly(false);
-            joinTypeCookie.setMaxAge(60 * 60);
-
-            response.addCookie(joinTypeCookie);
-
-            response.sendRedirect(oAuthClientAuth);
-        }
-
+      response.addCookie(cookie);
+      response.sendRedirect(oAuthClientMain);
     }
+    // description: 회원가입 X //
+    else {
+      String snsId = (String) attributes.get("snsId");
+      Cookie snsIdCookie = new Cookie("snsId", snsId);
+      snsIdCookie.setPath("/");
+      snsIdCookie.setHttpOnly(false);
+      snsIdCookie.setMaxAge(60 * 60);
+
+      response.addCookie(snsIdCookie);
+      
+      String joinType = (String) attributes.get("joinType");
+      Cookie joinTypeCookie = new Cookie("joinType", joinType);
+      joinTypeCookie.setPath("/");
+      joinTypeCookie.setHttpOnly(false);
+      joinTypeCookie.setMaxAge(60 * 60);
+      
+      response.addCookie(joinTypeCookie);
+
+      response.sendRedirect(oAuthClientAuth);
+    }
+  }
 
 }
