5fdce76a6d81b750c2812d6ce856c29d50ecd2c8
yuki-k1313 <yuki1313@naver.com>
2025-03-26 16:17:57 +0900

    2025-03-24

--- src/main/java/com/jmg/memories_back/common/entity/ConcentrationTestEntity.java
+++ src/main/java/com/jmg/memories_back/common/entity/ConcentrationTestEntity.java
@@ -23,37 +23,37 @@ import lombok.Setter;
 @AllArgsConstructor
 @IdClass(ConcentrationTestPk.class)
 public class ConcentrationTestEntity {
-    @Id
-    private String userId;
-    @Id
-    private Integer sequence;
-    private Integer measurementScore;
-    private Integer errorCount;
-    private String testDate;
-    private Integer scoreGap;
-    private Integer errorGap;
+  @Id
+  private String userId;
+  @Id
+  private Integer sequence;
+  private Integer measurementScore;
+  private Integer errorCount;
+  private String testDate;
+  private Integer scoreGap;
+  private Integer errorGap;
 
-    public ConcentrationTestEntity(PostConcentrationRequestDto dto, String userId) {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+  public ConcentrationTestEntity(PostConcentrationRequestDto dto, String userId) {
+    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-        this.userId = userId;
-        this.sequence = 1;
-        this.measurementScore = dto.getMeasurementScore();
-        this.errorCount = dto.getErrorCount();
-        this.testDate = now.format(dateTimeFormatter);
-    }
+    this.userId = userId;
+    this.sequence = 1;
+    this.measurementScore = dto.getMeasurementScore();
+    this.errorCount = dto.getErrorCount();
+    this.testDate = now.format(dateTimeFormatter);
+  }
 
-    public ConcentrationTestEntity(PostConcentrationRequestDto dto, ConcentrationTestEntity preEntity, String userId) {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+  public ConcentrationTestEntity(PostConcentrationRequestDto dto, ConcentrationTestEntity preEntity, String userId) {
+    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-        this.userId = userId;
-        this.sequence = preEntity.getSequence() + 1;
-        this.measurementScore = dto.getMeasurementScore();
-        this.errorCount = dto.getErrorCount();
-        this.testDate = now.format(dateTimeFormatter);
-        this.scoreGap = dto.getMeasurementScore() - preEntity.getMeasurementScore();
-        this.errorGap = dto.getErrorCount() - preEntity.getErrorCount();
-    }
+    this.userId = userId;
+    this.sequence = preEntity.getSequence() + 1;
+    this.measurementScore = dto.getMeasurementScore();
+    this.errorCount = dto.getErrorCount();
+    this.testDate = now.format(dateTimeFormatter);
+    this.scoreGap = dto.getMeasurementScore() - preEntity.getMeasurementScore();
+    this.errorGap = dto.getErrorCount() - preEntity.getErrorCount();
+  }
 }
