61c5aead8cd6d968b9aa78249954abfd41dc0d63
yuki-k1313 <yuki1313@naver.com>
2025-03-26 16:17:00 +0900

    2025-03-20

--- src/main/java/com/jmg/memories_back/common/entity/UserEntity.java
+++ src/main/java/com/jmg/memories_back/common/entity/UserEntity.java
@@ -15,41 +15,39 @@ import lombok.Setter;
 @Table(name="user")
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
+  
+  @Id
+  private String userId;
+  private String userPassword;
+  private String joinType;
+  private String snsId;
+  private String name;
+  private String address;
+  private String detailAddress;
+  private String profileImage;
+  private String gender;
+  private Integer age;
 
-    @Id
-    private String userId;
-    private String userPassword;
-    private String joinType;
-    private String snsId;
-    private String name;
-    private String address;
-    private String detailAddress;
-    private String profileImage;
-    private String gender;
-    private Integer age;
+  public UserEntity(SignUpRequestDto dto) {
+    this.userId = dto.getUserId();
+    this.userPassword = dto.getUserPassword();
+    this.name = dto.getName();
+    this.address = dto.getAddress();
+    this.detailAddress = dto.getDetailAddress();
+    this.joinType = dto.getJoinType();
+    this.snsId = dto.getSnsId();
+  }
 
-    public UserEntity(SignUpRequestDto dto) {
-
-        this.userId = dto.getUserId();
-        this.userPassword = dto.getUserPassword();
-        this.name = dto.getName();
-        this.address = dto.getAddress();
-        this.detailAddress = dto.getDetailAddress();
-        this.joinType = dto.getJoinType();
-        this.snsId = dto.getSnsId();
-        
-    }
-
-    public void patch(PatchUserRequestDto dto) {
-        this.name = dto.getName();
-        this.profileImage = dto.getProfileImage();
-        this.address = dto.getAddress();
-        this.detailAddress = dto.getDetailAddress();
-        this.gender = dto.getGender();
-        this.age = dto.getAge();
-    }
+  public void patch(PatchUserRequestDto dto) {
+    this.name = dto.getName();
+    this.profileImage = dto.getProfileImage();
+    this.address = dto.getAddress();
+    this.detailAddress = dto.getDetailAddress();
+    this.gender = dto.getGender();
+    this.age = dto.getAge();
+  }
 
 }
