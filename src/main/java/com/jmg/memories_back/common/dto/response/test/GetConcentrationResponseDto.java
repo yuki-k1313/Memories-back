c1c8858351eac158b6bc29bb46908ec4735f1f05
yuki-k1313 <yuki1313@naver.com>
2025-03-26 16:01:35 +0900

    2025-03-24

--- src/main/java/com/jmg/memories_back/common/dto/response/test/GetConcentrationResponseDto.java
+++ src/main/java/com/jmg/memories_back/common/dto/response/test/GetConcentrationResponseDto.java
@@ -7,21 +7,22 @@ import org.springframework.http.ResponseEntity;
 
 import com.jmg.memories_back.common.dto.response.ResponseDto;
 import com.jmg.memories_back.common.entity.ConcentrationTestEntity;
-import com.jmg.memories_back.common.vo.ConcentrationTestVo;
+import com.jmg.memories_back.common.vo.ConcentrationTestVO;
 
 import lombok.Getter;
 
 @Getter
 public class GetConcentrationResponseDto extends ResponseDto {
+  
+  private List<ConcentrationTestVO> concentrationTests;
 
-	private List<ConcentrationTestVo> concentrationTests;
-	private GetConcentrationResponseDto(List<ConcentrationTestEntity> concentrationTestEntities) {
-		this.concentrationTests = ConcentrationTestVo.getList(concentrationTestEntities);
-	}
+  private GetConcentrationResponseDto(List<ConcentrationTestEntity> concentrationTestEntities) {
+    this.concentrationTests = ConcentrationTestVO.getList(concentrationTestEntities);
+  }
 
-	public static ResponseEntity<GetConcentrationResponseDto> success(List<ConcentrationTestEntity> concentrationTestEntities) {
-		GetConcentrationResponseDto body = new GetConcentrationResponseDto(concentrationTestEntities);
-		return ResponseEntity.status(HttpStatus.OK).body(body);
-	}
+  public static ResponseEntity<GetConcentrationResponseDto> success(List<ConcentrationTestEntity> concentrationTestEntities) {
+    GetConcentrationResponseDto body = new GetConcentrationResponseDto(concentrationTestEntities);
+    return ResponseEntity.status(HttpStatus.OK).body(body);
+  }
 
 }
